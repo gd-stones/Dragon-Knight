516f83a8c8cd02167ae91e768fc29bf0b186c147
nguyen-duc-viet <61795479+nguyen-duc-viet@users.noreply.github.com>
2023-05-16 17:03:46 +0700

    #7 HEALTH SYSTEM

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -19,11 +19,11 @@ public class CameraController : MonoBehaviour
     {
         //Room camera
         //print(currentposX);
-        //transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentposX, transform.position.y, transform.position.z), ref velocity, speed);
+        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentposX, transform.position.y, transform.position.z), ref velocity, speed);
 
         //Follow player
-        transform.position = new Vector3(player.position.x + lookAhead, transform.position.y, transform.position.z);
-        lookAhead = Mathf.Lerp(lookAhead, (aheadDistance * player.localScale.x), Time.deltaTime * cameraSpeed);
+        //transform.position = new Vector3(player.position.x + lookAhead, transform.position.y, transform.position.z);
+        //lookAhead = Mathf.Lerp(lookAhead, (aheadDistance * player.localScale.x), Time.deltaTime * cameraSpeed);
     }
 
     public void MoveToNewRoom(Transform _newRoom)
